f3ea16cfc32e4a5837e5f05f835244079af869c1
JackHasSoup <jack.sneddon1@gmail.com>
2025-07-15 16:07:16 +0100

    fixed polished stone name not appearing

--- src/main/java/jackhassoup/scaffold/blocks/BlockLogicPlatform.java
+++ src/main/java/jackhassoup/scaffold/blocks/BlockLogicPlatform.java
@@ -8,7 +8,6 @@ import net.minecraft.core.block.material.Material;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.util.phys.AABB;
-import net.minecraft.core.util.phys.Vec3;
 import net.minecraft.core.world.World;
 import net.minecraft.core.world.WorldSource;
 import turniplabs.halplibe.helper.EnvironmentHelper;
